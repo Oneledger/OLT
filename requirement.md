ab800c6a91a70012cded18f01e9209de4c8dc098
Yang Li <stephen.liy@gmail.com>
2018-03-11 22:02:28 -0400

    add more test and prepare the document for timelocker

--- requirement.md
+++ requirement.md
@@ -1,15 +1,14 @@
-#Token Distribution requirement
+# Token Distribution requirement
 
 ## Concept
 
 Token Name: OLT
 Total Number: 100000000 * (10 ** decimal)
 Distribution method:
-
-    - Team reserve: locked for 12 months with quarterly cliffs
-    - Advisor reserve: locked for 6 months with monthly release
-    - Company reserve: locked for 18 months with montly release starting from the seventh month
-    - Others?
+- Team reserve: locked for 12 months with quarterly cliffs
+- Advisor reserve: locked for 6 months with monthly release
+- Company reserve: locked for 18 months with monthly release starting from the seventh month
+- Others?
 
 ## Technical Requirement
 
--- test/tokenTest.js
+++ test/tokenTest.js
@@ -1,15 +1,30 @@
 var OneledgerToken = artifacts.require('./OneledgerToken.sol')
 
-contract('OneledgerToken', function([owner]){
+contract('OneledgerToken', function([owner,investor1, investor2, spender]){
   let token = null
+  const expectedTotalSupply = 100000000 * (10 ** 18);
   beforeEach(async ()=>{
-    token = await OneledgerToken.deployed()
-  })
+    token = await OneledgerToken.new()
+  });
   it('has an owner',async ()=>{
     assert.equal(await token.owner(), owner);
-  })
-  it("should has total supply of 100000000 * (10 ** decimals) from the contract", async ()=>{
+  });
+  it('should has total supply of 100000000 * (10 ** decimals) from the contract', async ()=>{
       const totalSupply = await token.totalSupply();
-      assert(totalSupply, 100000000 * (10 ** 18));
-  })
+      assert(totalSupply, expectedTotalSupply);
+  });
+  it('should be able to transfer the token to the user', async () => {
+    await token.transfer(investor1, 1000);
+    assert.equal(await token.balanceOf(investor1), 1000);
+    assert.equal(await token.balanceOf(owner), expectedTotalSupply - 1000);
+  });
+  it('should be able to transferFrom the token from one user to another user', async ()=>{
+    await token.transfer(investor1, 1000);
+    assert.equal(await token.balanceOf(investor1), 1000);
+    await token.approve(spender, 100, {from: investor1});
+    await token.transferFrom(investor1, investor2, 100, {from: spender});
+    assert.equal(await token.balanceOf(investor1), 900);
+    assert.equal(await token.balanceOf(investor2), 100);
+  });
+
 })
