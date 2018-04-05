f02666a94352cf9792684dd51e9860b885360966
Roy Blankman <riblankman@gmail.com>
2018-04-05 05:53:31 -0400

    specify compiler optimization options explicitly

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
 };
