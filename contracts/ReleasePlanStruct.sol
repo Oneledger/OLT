e60006d984006b298a4cbaaf367f5cce2f4510ba
Roy Blankman <riblankman@gmail.com>
2018-04-08 16:10:42 -0400

    clarifying TimeLocker comment

--- contracts/ReleasePlanStruct.sol
+++ contracts/ReleasePlanStruct.sol
@@ -3,7 +3,7 @@ pragma solidity 0.4.21;
 library ReleasePlanStruct {
   struct TimeLocker {
     uint256 releaseTime;
-    uint256 frozenTokens; //the token frozen before release time
+    uint256 frozenTokens; // the number of tokens frozen until 'releaseTime'
   }
   struct ReleasePlan {
     bool initialized;
