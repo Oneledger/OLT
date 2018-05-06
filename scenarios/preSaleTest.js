d393ac85627a9e0939e38beadeb03a4f8184ac75
Roy Blankman <riblankman@gmail.com>
2018-05-06 16:47:58 -0400

    cleaning up preSaleTest.js

--- scenarios/preSaleTest.js
+++ scenarios/preSaleTest.js
@@ -13,23 +13,23 @@ require('chai')
     let ico;
 
     beforeEach(async () => {
-      let weiCap = 10000 * (10 ** 18);
+      let weiCap = web3.toWei(10000);
       let ratePerWei = 9668; // convert to rate per wei
-      ico = await ICO.new(wallet,ratePerWei,latestTime(), weiCap);
+      ico = await ICO.new(wallet, ratePerWei, latestTime(), weiCap);
       token = OneledgerToken.at(await ico.token());
-      await ico.addToWhiteList([user], 4.8 * (10 ** 18));
+      await ico.addToWhiteList([user], web3.toWei(4.8));
     });
 
     it('should not allow to buy new token when ICO contract is closed', async () => {
       await ico.closeSale();
-      await ico.sendTransaction({from: user, value: 4.8 * (10 ** 18)}).should.be.rejectedWith('revert');
+      await ico.sendTransaction({from: user, value: web3.toWei(4.8)}).should.be.rejectedWith('revert');
       let balanceOf = await token.balanceOf(web3.eth.coinbase);
-      assert.equal(balanceOf.toNumber(), 1000000000 * (10 ** 18))
+      assert.equal(balanceOf.toNumber(), web3.toWei(1000000000))
     });
 
     it('should be able to allow deploy the vesting contract', async () => {
-      let totalToken = 1933701 * (10**18);
+      let totalToken = web3.toWei(1933701);
       await OneledgerTokenVesting.new(advisor, latestTime() + duration.minutes(10),
-                                      duration.weeks(4), totalToken/12).should.be.fulfilled;
+                                      duration.weeks(4), totalToken / 12).should.be.fulfilled;
     })
   })
